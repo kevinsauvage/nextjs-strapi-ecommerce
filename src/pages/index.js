ca2f7229b75af879a3ba572d41d5e67f6ea16002
Kevin Sauvage <kevinsauvage@outlook.com>
2022-09-18 16:21:14 +0200

    commit changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import SecureBanner from '@/components/SecureBanner/SecureBanner';
 import Banner1 from '@/components/BannerHome/Banner1';
 import Banner2 from '@/components/BannerHome/Banner2';
-import Banner3 from '@/components/BannerHome/Banner3';
 import styles from '@/styles/Home.module.scss';
 import Container from '@/components/Container/Container';
 import { getLastOrderedProducts } from '@/lib/algolia';
@@ -13,7 +12,6 @@ export default function Home({ lastOrderedProduct }) {
       <Container>
         <Banner1 />
         <Banner2 />
-        <Banner3 />
       </Container>
       <SecureBanner />
     </div>
