d7f81d19f962d600f48cdc8fa3f12fe630640240
Kevin Sauvage <kevinsauvage@outlook.com>
2022-09-27 21:41:25 +0200

    add container to Banner

--- src/components/BannerHome/Banner1.js
+++ src/components/BannerHome/Banner1.js
@@ -1,6 +1,7 @@
 import Button from '@/components/Button/Button';
 import FlexColumn from '@/components/FlexColumn/FlexColumn';
 import config from '@/config/index';
+import Container from '../Container/Container';
 import styles from './Banner1.module.scss';
 
 export default function BannerHome() {
@@ -9,19 +10,21 @@ export default function BannerHome() {
       className={styles.banner}
       style={{ backgroundImage: `url(${config.homeBanner.imageUrl})` }}
     >
-      <FlexColumn className={styles.container}>
-        {config.homeBanner.upTitle && (
-          <p className={styles.upTitle}>{config.homeBanner.upTitle}</p>
-        )}
-        <h1 className={styles.title}>{config.homeBanner.title}</h1>
-        <p className={styles.subtitle}>{config.homeBanner.subtitle}</p>
-        <Button
-          text={config.homeBanner.buttonText}
-          quaternary
-          extraClass={styles.btn}
-          href={config.homeBanner.link}
-        />
-      </FlexColumn>
+      <Container>
+        <FlexColumn className={styles.container}>
+          {config.homeBanner.upTitle && (
+            <p className={styles.upTitle}>{config.homeBanner.upTitle}</p>
+          )}
+          <h1 className={styles.title}>{config.homeBanner.title}</h1>
+          <p className={styles.subtitle}>{config.homeBanner.subtitle}</p>
+          <Button
+            text={config.homeBanner.buttonText}
+            quaternary
+            extraClass={styles.btn}
+            href={config.homeBanner.link}
+          />
+        </FlexColumn>
+      </Container>
     </section>
   );
 }
