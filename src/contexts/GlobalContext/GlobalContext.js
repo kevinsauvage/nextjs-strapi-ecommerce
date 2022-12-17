c76d40af95c3850e0fbc8238a8d104b99c697de3
Kevin Sauvage <kevinsauvage@outlook.com>
2022-12-17 21:14:34 +0100

    fix

--- src/contexts/GlobalContext/GlobalContext.js
+++ src/contexts/GlobalContext/GlobalContext.js
@@ -33,7 +33,7 @@ export function GlobalProvider({ children }) {
   }, [router.asPath, resetToggle]);
 
   useEffect(() => {
-    if (checkoutOpen || userOpen || selectedProduct) {
+    if (checkoutOpen || searchOpen || userOpen || selectedProduct) {
       document.body.style.overflow = 'hidden';
     } else document.body.style.overflow = 'visible';
   }, [checkoutOpen, searchOpen, userOpen, selectedProduct]);
