5d027770e92f0bd5b7cfd682826932970cbaeb69
Kevin Sauvage <kevinsauvage@outlook.com>
2023-01-08 21:42:05 +0100

    fix

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -11,70 +11,60 @@ function CartPage() {
   const { checkout, handleQuantityChange, removeFromCheckout } =
     useCheckoutContext();
 
+  if (checkout?.lineItems?.length === 0) return <EmptyCart />;
+
   return (
     <Page title="Your Cart">
       <div className={styles.cart}>
-        {Array.isArray(checkout?.lineItems) &&
-        checkout?.lineItems?.length > 0 ? (
-          <>
-            <table className={styles.table}>
-              <thead className={styles.head}>
-                <tr>
-                  <th className="image text-left">Item</th>
-                  <th className="price">Price</th>
-                  <th className="qty">Quantity</th>
-                  <th className="total">Total</th>
-                  <th className="remove">Remove</th>
-                </tr>
-              </thead>
-              <tbody>
-                {checkout.lineItems.map((item) => (
-                  <CartItem
-                    key={item?.id}
-                    product={item.variant.product}
-                    collection={item.variant.product?.collections?.nodes?.[0]}
-                    variant={item.variant}
-                    quantity={item?.quantity}
-                    title={item?.title}
-                    removeFromCart={removeFromCheckout}
-                    lineId={item.id}
-                    handleQuantityChange={handleQuantityChange}
-                  />
-                ))}
-              </tbody>
-              <tfoot>
-                <tr className={styles.summary}>
-                  <td colSpan="4">
-                    <input style={{ display: 'none', visibility: 'hidden' }} />
-                  </td>
-                  <td className={styles.price}>
-                    <span>
-                      {checkout?.totalPrice?.amount} {checkout?.currencyCode}
-                    </span>
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
-            <div>
-              <Card title="Shipping Address">
-                <Address
-                  displayButton={false}
-                  address={checkout.shippingAddress}
+        <div>
+          <table className={styles.table}>
+            <thead className={styles.head}>
+              <tr>
+                <th className="image text-left">Item</th>
+                <th className="price">Price</th>
+                <th className="qty">Quantity</th>
+                <th className="total">Total</th>
+                <th className="remove">Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {checkout.lineItems.map((item) => (
+                <CartItem
+                  key={item?.id}
+                  product={item.variant.product}
+                  collection={item.variant.product?.collections?.nodes?.[0]}
+                  variant={item.variant}
+                  quantity={item?.quantity}
+                  title={item?.title}
+                  removeFromCart={removeFromCheckout}
+                  lineId={item.id}
+                  handleQuantityChange={handleQuantityChange}
                 />
-              </Card>
-              <div className={styles.btns}>
-                <CheckoutBtn
-                  extraClass={styles.btn}
-                  amount={checkout?.totalPrice?.amount}
-                  currencyCode={checkout?.currencyCode}
-                  url={checkout?.webUrl}
-                />
-              </div>
-            </div>
-          </>
-        ) : (
-          <EmptyCart />
-        )}
+              ))}
+            </tbody>
+            <tfoot>
+              <tr className={styles.summary}>
+                <td className={styles.price} colSpan="6">
+                  <span>
+                    Total {checkout?.totalPrice?.amount}{' '}
+                    {checkout?.currencyCode}
+                  </span>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div>
+          <Card title="Shipping Address">
+            <Address displayButton={false} address={checkout.shippingAddress} />
+          </Card>
+          <CheckoutBtn
+            extraClass={styles.btn}
+            amount={checkout?.totalPrice?.amount}
+            currencyCode={checkout?.currencyCode}
+            url={checkout?.webUrl}
+          />
+        </div>
       </div>
     </Page>
   );
