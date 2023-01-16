49808108b80b40baa1abd5cc0fa3ae38c4795fc8
Kevin Sauvage <kevinsauvage@outlook.com>
2023-01-16 20:24:44 +0100

    refactor collection context

--- src/contexts/CollectionContext/CollectionContext.js
+++ src/contexts/CollectionContext/CollectionContext.js
@@ -22,33 +22,22 @@ export function CollectionProvider({ children }) {
   } = states;
 
   const { toggleFilter } = useGlobalContext();
-
   const { query, pathname, push, asPath, href } = useRouter();
 
-  // Check if there is new filters to apply
   const isSelectionDifferent = useCallback(() => {
     if (notAppliedFilters.length) return true;
     if (selectedFilters.length !== actualFilters.length) return true;
-
     return false;
   }, [actualFilters.length, notAppliedFilters.length, selectedFilters.length]);
 
-  const setPageInfo = useCallback((payload) => {
-    dispatch({ type: actions.SET_PAGE_INFO, payload });
-  }, []);
-
-  const setProducts = useCallback((payload) => {
-    dispatch({ type: actions.SET_PRODUCTS, payload });
-  }, []);
-
-  const setAllFilters = useCallback((payload) => {
-    dispatch({ type: actions.SET_ALL_FILTERS, payload });
-  }, []);
+  const setPageInfo = useCallback((payload) => dispatch({ type: actions.SET_PAGE_INFO, payload }), []);
+  const setProducts = useCallback((payload) => dispatch({ type: actions.SET_PRODUCTS, payload }), []);
+  const setAllFilters = useCallback((payload) => dispatch({ type: actions.SET_ALL_FILTERS, payload }), []);
+  const handleSetLayout = useCallback((data) => dispatch({ type: actions.SET_LAYOUT, payload: data }), []);
 
   const setSelectedFilters = useCallback(
     (payload) => {
-      const newFilters = [...selectedFilters, payload];
-      dispatch({ type: actions.SET_SELECTED_FILTERS, payload: newFilters });
+      dispatch({ type: actions.SET_SELECTED_FILTERS, payload: [...selectedFilters, payload] });
     },
     [selectedFilters]
   );
@@ -61,71 +50,63 @@ export function CollectionProvider({ children }) {
     [selectedFilters]
   );
 
+  const handleGetData = useCallback(
+    async (first, filters, sort, after = null) => {
+      dispatch({ type: actions.SET_LOADING, payload: true });
+      const data = await filterCollectionForward(query.collectionSlug || 'all', first, filters, sort, after);
+      dispatch({ type: actions.SET_LOADING, payload: false });
+      return data;
+    },
+    [query.collectionSlug]
+  );
+
+  const handleSetFilterState = useCallback(
+    (data, concat = false) => {
+      const {
+        collection: { products: newProducts },
+        pageInfo: newPageInfo,
+      } = data || {};
+
+      if (newPageInfo) setPageInfo(newPageInfo);
+      if (newProducts) setProducts(concat ? [...products, ...newProducts] : newProducts);
+      if (!concat) window.scrollTo(0, 0);
+    },
+    [products, setPageInfo, setProducts]
+  );
+
   const resetFilters = useCallback(async () => {
     toggleFilter(false);
-
-    dispatch({ type: actions.SET_LOADING, payload: true });
-
     const path = pathname.replace('[collectionSlug]', query.collectionSlug);
     const newUrl = new URL(config.baseUrl + path);
-
-    dispatch({ type: actions.SET_SELECTED_FILTERS, payload: [] });
     push(newUrl, undefined, { shallow: true });
-
-    const data = await filterCollectionForward(query.collectionSlug, 10, [], query.sort_key, null);
-
-    dispatch({ type: actions.SET_LOADING, payload: false });
-
-    if (data) {
-      const newProducts = data?.collection?.products;
-      const newPageInfo = data?.pageInfo;
-      if (newProducts) setProducts(newProducts);
-      if (newPageInfo) setPageInfo(newPageInfo);
-      window.scrollTo(0, 0);
-    }
-    return null;
-  }, [pathname, push, query.collectionSlug, query.sort_key, setPageInfo, setProducts, toggleFilter]);
+    const data = await handleGetData(15, [], query.sort_key, null);
+    handleSetFilterState(data);
+  }, [
+    handleGetData,
+    handleSetFilterState,
+    pathname,
+    push,
+    query.collectionSlug,
+    query.sort_key,
+    toggleFilter,
+  ]);
 
   const applyFilters = useCallback(async () => {
     toggleFilter(false);
     const path = pathname.replace('[collectionSlug]', query.collectionSlug);
     const newUrl = new URL(config.baseUrl + path);
-
     if (selectedFilters.length > 0) {
-      selectedFilters.forEach((item) => {
-        newUrl.searchParams.append('filter', item.id);
-      });
+      selectedFilters.forEach((item) => newUrl.searchParams.append('filter', item.id));
     } else newUrl.searchParams.delete('filter');
-
     if (query.sort_key) newUrl.searchParams.set('sort_key', query.sort_key);
-
-    if (isSelectionDifferent()) {
-      if (newUrl.href === href) return;
-    }
+    if (isSelectionDifferent() && newUrl.href === href) return;
     push(newUrl, undefined, { shallow: true });
-
-    dispatch({ type: actions.SET_LOADING, payload: true });
-
     const filters = selectedFilters.map((item) => JSON.parse(item.input));
-
-    const data = await filterCollectionForward(
-      query.collectionSlug || 'all',
-      10,
-      filters,
-      query.sort_key,
-      null
-    );
-
-    dispatch({ type: actions.SET_LOADING, payload: false });
-
-    if (data) {
-      const newProducts = data?.collection?.products;
-      const newPageInfo = data?.pageInfo;
-      if (newProducts) setProducts(newProducts);
-      if (newPageInfo) setPageInfo(newPageInfo);
-      window.scrollTo(0, 0);
-    }
+    const data = await handleGetData(15, filters, query.sort_key, null);
+    handleSetFilterState(data);
   }, [
+    handleGetData,
+    handleSetFilterState,
     href,
     isSelectionDifferent,
     pathname,
@@ -133,8 +114,6 @@ export function CollectionProvider({ children }) {
     query.collectionSlug,
     query.sort_key,
     selectedFilters,
-    setPageInfo,
-    setProducts,
     toggleFilter,
   ]);
 
@@ -143,90 +122,41 @@ export function CollectionProvider({ children }) {
       if (!value) return null;
       const newUrl = new URL(config.baseUrl + asPath);
       newUrl.searchParams.set('sort_key', value);
-      dispatch({ type: actions.SET_LOADING, payload: true });
-
+      push(newUrl, undefined, { shallow: true });
       const filters = actualFilters.map((filter) => JSON.parse(filter.input));
-
-      const data = await filterCollectionForward(query.collectionSlug || 'all', 10, filters, value, null);
-
-      dispatch({ type: actions.SET_LOADING, payload: false });
-
-      if (data) {
-        const newProducts = data?.collection?.products;
-        const newPageInfo = data?.pageInfo;
-        if (newProducts) setProducts(newProducts);
-        if (newPageInfo) setPageInfo(newPageInfo);
-        window.scrollTo(0, 0);
-        return push(newUrl, undefined, { shallow: true });
-      }
-      return null;
+      const data = await handleGetData(15, filters, value, null);
+      return handleSetFilterState(data);
     },
-    [actualFilters, asPath, push, query.collectionSlug, setPageInfo, setProducts]
+    [actualFilters, asPath, handleGetData, handleSetFilterState, push]
   );
 
-  const handleSetLayout = useCallback((newLayout) => {
-    dispatch({ type: actions.SET_LAYOUT, payload: newLayout });
-  }, []);
-
-  // PAGINATION ==============================================================================================
-
-  /* A function that is called when the user clicks on the next button. */
   const handleNext = useCallback(async () => {
-    dispatch({ type: actions.SET_LOADING, payload: true });
-
     const filteredFilters = getFiltersFromQuery(allFilters, query);
     const filters = filteredFilters.map((item) => JSON.parse(item.input));
+    const data = await handleGetData(15, filters, query.sort_key, pageInfo.endCursor);
+    handleSetFilterState(data, true);
+  }, [allFilters, handleGetData, handleSetFilterState, pageInfo.endCursor, query]);
 
-    const data = await filterCollectionForward(
-      query.collectionSlug || 'all',
-      10,
-      filters,
-      query.sort_key,
-      pageInfo.endCursor
-    );
-
-    dispatch({ type: actions.SET_LOADING, payload: false });
-
-    if (data) {
-      const newProducts = data?.collection?.products;
-      const newPageInfo = data?.pageInfo;
-      if (newProducts) setProducts([...products, ...newProducts]);
-      if (newPageInfo) setPageInfo(newPageInfo);
-    }
-  }, [allFilters, pageInfo.endCursor, products, query, setPageInfo, setProducts]);
-
-  // EFFECTS ==================================================================================================
-
-  // Reset filters if collection change
   useEffect(() => {
-    dispatch({
-      type: actions.SET_SELECTED_FILTERS,
-      payload: [],
-    });
+    dispatch({ type: actions.SET_SELECTED_FILTERS, payload: [] });
   }, [query.collectionSlug]);
 
-  // Get filter from query and set it in selected filters
   useEffect(() => {
     if (query?.filter) {
       const filteredFilters = getFiltersFromQuery(allFilters, query);
       if (Array.isArray(filteredFilters)) {
-        dispatch({
-          type: actions.SET_SELECTED_FILTERS,
-          payload: filteredFilters,
-        });
+        dispatch({ type: actions.SET_SELECTED_FILTERS, payload: filteredFilters });
       }
     }
   }, [query, allFilters]);
 
   useEffect(() => {
     const values = getFiltersFromQuery(allFilters, query);
-
     dispatch({ type: actions.SET_ACTUAL_FILTERS, payload: values });
   }, [allFilters, query]);
 
   useEffect(() => {
     const result = selectedFilters.filter((obj) => actualFilters.every((s) => s.id !== obj.id));
-
     dispatch({ type: actions.SET_NOT_APPLIED_FILTERS, payload: result });
   }, [actualFilters, selectedFilters]);
 
