d1d66a01c86deba1710fb7dbc23ca08e61776572
Kevin Sauvage <kevinsauvage@outlook.com>
2023-01-18 22:39:46 +0100

    fix

--- src/layout/RootLayout/RootLayout.js
+++ src/layout/RootLayout/RootLayout.js
@@ -1,12 +1,13 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { useEffect, useState } from 'react';
 import { getMenuFooter, getMenuHeader, getShop } from '@/lib/shopify/shop/shopApiCall';
 import useGlobalContext from '@/contexts/GlobalContext/useGlobalContext';
 import Cart from '@/components/_slides/Cart/Cart';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import ModalProduct from '@/components/_modals/modalProduct/ModalProduct';
 import SecureBanner from '@/components/_banners/SecureBanner/SecureBanner';
 import PageLoader from '@/components/_loaders/PageLoader/PageLoader';
+import dynamic from 'next/dynamic';
 import styles from './RootLayout.module.scss';
 
 function RootLayout({ children }) {
@@ -16,27 +17,31 @@ function RootLayout({ children }) {
   const [shopInfo, setShopInfo] = useState();
 
   useEffect(() => {
-    console.time('Function #1');
-
     const fetchData = async () => {
       const [header, footer, shop] = await Promise.all([getMenuHeader(), getMenuFooter(), getShop()]);
       setMenuFooter(footer);
       setMenuHeader(header);
       setShopInfo(shop);
-      console.timeEnd('Function #1');
     };
 
     fetchData();
   }, []);
+
+  const loader = <PageLoader />;
+
+  const getProductModal = () => {
+    const ProductModal = dynamic(() => import('../../components/_modals/modalProduct/ModalProduct'), {
+      loading: () => loader,
+    });
+    return <ProductModal handleClose={() => setSelectedProduct(false)} product={selectedProduct} />;
+  };
 
   return (
     <div className={styles.RootLayout}>
       <Cart />
       {loading && <PageLoader />}
       <Header headerMenu={menuHeader} />
-      {selectedProduct ? (
-        <ModalProduct handleClose={() => setSelectedProduct(false)} product={selectedProduct} />
-      ) : null}
+      {selectedProduct && getProductModal()}
       {children}
       <SecureBanner />
       <Footer menuFooter={menuFooter} shopInfo={shopInfo} />
