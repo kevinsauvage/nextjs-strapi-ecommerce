721633d442f32e142ae85ac015b39d761fe40b12
Kevin Sauvage <kevinsauvage@outlook.com>
2023-01-22 18:52:11 +0100

    fix sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -3,14 +3,19 @@ import { getCollections } from '@/lib/shopify/collection/collectionApiCall';
 import config from '@/config/index';
 
 function Sitemap() {
-  return null;
+  return {};
 }
 
 export const getServerSideProps = async ({ res }) => {
   const BASE_URL = config.baseUrl;
 
   const staticPaths = fs
-    .readdirSync('./src/pages')
+    .readdirSync(
+      {
+        development: './src/pages',
+        production: './',
+      }[process.env.NODE_ENV]
+    )
     .filter(
       (staticPage) => !['sitemap.xml.js', '404.js', '_app.js', '_document.js', 'api'].includes(staticPage)
     )
@@ -19,7 +24,7 @@ export const getServerSideProps = async ({ res }) => {
   const collections = await getCollections(50);
 
   const dynamicPaths = collections.map(
-    (collection) => `${BASE_URL}/${config.routes.collection}/${collection.handle}`
+    (collection) => `${BASE_URL}${config.routes.collection}/${collection.handle}`
   );
 
   const allPaths = [...staticPaths, ...dynamicPaths];
@@ -29,10 +34,13 @@ export const getServerSideProps = async ({ res }) => {
       // This is where we would be putting in our URLs
       ${allPaths
         .map(
-          (url) => `
-            <url>
-              <loc>${url}</loc>
-            </url>
+          (url) =>
+            `<url>
+            <loc>${url}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+          </url>
           `
         )
         .join('')}
