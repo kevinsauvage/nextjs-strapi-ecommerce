1855afe5a7657e984cd12e2666c15ab9bdf63f1c
Kevin Sauvage <kevinsauvage@outlook.com>
2023-02-12 01:42:02 +0100

    fix

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,16 +1,53 @@
+import { useEffect, useRef, useState } from 'react';
+import { MdClose } from 'react-icons/md';
 import styles from './Slide.module.scss';
 
-function Slide({ isOpen, handleClose, content }) {
-  return (
-    <div className={`${isOpen && styles.open} ${styles.slide}`}>
-      <div className={`${isOpen && styles.openInner} ${styles.inner}`}>
-        <button type="button" onClick={handleClose}>
-          close
-        </button>
-        {content}
-      </div>
-    </div>
-  );
-}
+export default function Slide({ isOpen, handleClose, title, footer, content }) {
+  const footerRef = useRef(null);
+  const [contentHeight, setContentHeight] = useState();
+  const headerHeight = 50;
 
-export default Slide;
+  useEffect(() => {
+    if (footerRef?.current) {
+      const rect = footerRef?.current?.getBoundingClientRect();
+      setContentHeight(window.innerHeight - rect.height - headerHeight);
+    }
+  }, []);
+
+  return isOpen ? (
+    <div
+      role="button"
+      tabIndex="0"
+      className={`${styles.slide} fadeIn`}
+      onClick={handleClose}
+      onKeyDown={(event) => event.key === 'Escape' && handleClose}
+    >
+      <aside
+        role="presentation"
+        className={styles.outer}
+        onClick={(e) => e.stopPropagation()}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <header className={styles.header}>
+          <h4 className={styles.title}>{title}</h4>
+          <button type="button" className={styles.close} onClick={handleClose}>
+            <MdClose />
+          </button>
+        </header>
+        <div
+          className={styles.content}
+          style={{
+            maxHeight: `${contentHeight}px`,
+          }}
+        >
+          {content}
+        </div>
+        {footer && (
+          <footer ref={footerRef} className={styles.footer}>
+            {footer}
+          </footer>
+        )}
+      </aside>
+    </div>
+  ) : null;
+}
