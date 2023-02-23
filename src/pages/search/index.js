01be8e87218d2467876108afc8b753ee9eac80cd
Kevin Sauvage <kevinsauvage@outlook.com>
2023-02-23 21:32:42 +0100

    fix

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -2,9 +2,10 @@ import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
 import PageLayout from '@/layout/PageLayout/PageLayout';
 import ProductsList from '@/components/_scopes/product/ProductList/ProductsList';
-import PageLoader from '@/components/_loaders/PageLoader/PageLoader';
 import Container from '@/components/Container/Container';
 import getClient from '@/shopify/index';
+import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
+import Button from '@/components/Button/Button';
 import styles from './search.module.scss';
 
 function Search() {
@@ -15,20 +16,32 @@ function Search() {
 
   const searchTerm = query?.query;
 
-  const handleSearch = useCallback(async () => {
-    if (!searchTerm || !searchTerm?.trim()) return;
-    setLoading(true);
-    const searchResponse = await getClient().storefront.product.getProducts({
-      query: `${searchTerm}*`,
-      first: 10,
-    });
-    setLoading(false);
+  useEffect(() => {
+    setSearch([]);
+  }, [query?.query]);
 
-    const { products, pageInfo: pageInfoRes } = searchResponse || {};
+  const handleSearch = useCallback(
+    async (endCursor) => {
+      if (!searchTerm || !searchTerm?.trim()) return;
+      setLoading(true);
 
-    if (products) setSearch(products);
-    if (pageInfoRes) setPageInfo(pageInfoRes);
-  }, [searchTerm]);
+      const searchResponse = await getClient().storefront.product.getProducts({
+        query: `${searchTerm}*`,
+        first: 10,
+        after: endCursor || null,
+      });
+
+      console.log('🚀 ~ file: index.js:27 ~ searchResponse ~ searchResponse:', searchResponse);
+
+      setLoading(false);
+
+      const { products, pageInfo: pageInfoRes } = searchResponse || {};
+
+      if (products) setSearch((prev) => [...prev, ...products]);
+      if (pageInfoRes) setPageInfo(pageInfoRes);
+    },
+    [searchTerm]
+  );
 
   useEffect(() => {
     handleSearch();
@@ -36,12 +49,23 @@ function Search() {
 
   return (
     <PageLayout title="Search page">
-      {loading && <PageLoader />}
-      <Container>
-        <div className={styles.header}>
+      <Breadcrumbs />
+      <div className={styles.header}>
+        <Container>
           <h1>Search results for: {searchTerm?.toUpperCase()}</h1>
+        </Container>
+      </div>
+      <Container>
+        <div className={styles.search}>
+          <ProductsList loading={loading} layout="grid" products={search} />
+          {pageInfo?.hasNextPage && (
+            <div>
+              <Button primary onClick={() => handleSearch(pageInfo.endCursor)}>
+                Next
+              </Button>
+            </div>
+          )}
         </div>
-        <ProductsList layout="grid" products={search} />
       </Container>
     </PageLayout>
   );
