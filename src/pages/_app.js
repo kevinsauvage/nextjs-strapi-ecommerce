d4cdcc17e76c86c4a9b6545fa100335748ef591e
Kevin Sauvage <kevinsauvage@outlook.com>
2023-03-04 23:14:02 +0100

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,11 +24,6 @@ function MyApp({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           
-          gtag('consent', 'default', {
-            'ad_storage': 'denied',
-            'analytics_storage': 'denied'
-          });
-          
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
