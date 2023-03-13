a6ba07b6f578b81ca3b4408967603ae4a91043ac
Kevin Sauvage <kevinsauvage@outlook.com>
2023-03-13 22:04:21 +0100

    test

---
+++ pa11y-config.js
@@ -0,0 +1,11 @@
+const credentials = 'kevin:50625062';
+// eslint-disable-next-line no-buffer-constructor
+const encodedCredentials = new Buffer(credentials).toString('base64');
+
+module.exports = {
+  defaults: {
+    headers: {
+      Authorization: `Basic ${encodedCredentials}`,
+    },
+  },
+};
