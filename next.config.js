30d82106b3279dc7ce2009c678aef950833ea22c
Kevin Sauvage <kevinsauvage@outlook.com>
2023-03-25 00:26:05 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,33 @@
+/* eslint-disable unicorn/prefer-module */
 /** @type {import('next').NextConfig} */
-const path = require('path');
+
+const path = require('node:path');
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/404',
-        destination: '/',
-        permanent: true,
-      },
-    ];
+  compiler: {
+    // removeConsole: process.env.NODE_ENV === 'production',
   },
+  experimental: {
+    cpus: 1,
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+  },
+
   images: {
     domains: ['res.cloudinary.com', 'cdn.shopify.com'],
   },
-
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        destination: '/',
+        permanent: true,
+        source: '/404',
+      },
+    ];
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, './src/styles/')],
     prependData: `
@@ -23,17 +36,6 @@ const nextConfig = {
         @import "_responsive.scss";
         @import "_themes.scss";
         `,
-  },
-  experimental: {
-    // This is experimental but can
-    // be enabled to allow parallel threads
-    // with nextjs automatic static generation
-    workerThreads: false,
-    cpus: 1,
-  },
-  reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
