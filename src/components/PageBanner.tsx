bd3e5d71556fc495a9c70261ffdf0a44e7a140d8
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-05-05 22:32:19 +0200

    fix: allow custom className prop in PageBanner component for better styling flexibility

--- src/components/PageBanner.tsx
+++ src/components/PageBanner.tsx
@@ -2,12 +2,16 @@ const PageBanner = ({
   title,
   description,
   children,
+  className,
 }: {
   title: string;
   description?: string;
   children?: React.ReactNode;
+  className?: string;
 }) => (
-  <div className="container mx-auto flex flex-col items-center justify-center text-center py-12 px-4 space-y-8">
+  <div
+    className={`container mx-auto flex flex-col items-center justify-center text-center py-12 px-4 space-y-8 ${className}`}
+  >
     <h1 className="font-heading text-4xl">{title}</h1>
     {description && (
       <p className="text-muted-foreground mb-8 max-w-2xl line-clamp-4 overflow-hidden">
