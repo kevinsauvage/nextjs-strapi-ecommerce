d33bb8dfe9d76d58463faa0f466789cd92f4dced
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-05-09 20:30:09 +0200

    fix: add container class for consistent layout in collection page

--- src/app/collections/[collectionSlug]/layout.tsx
+++ src/app/collections/[collectionSlug]/layout.tsx
@@ -69,7 +69,7 @@ const Layout = async ({
         <Breadcrumbs />
         <CollectionNav collectionSlug={collectionSlug} items={findNavItems()} />
       </PageBanner>
-      <div className="px-4">{children}</div>
+      <div className="container mx-auto px-4">{children}</div>
     </div>
   );
 };
