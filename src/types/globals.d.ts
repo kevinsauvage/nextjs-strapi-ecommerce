e3252cb0821e5769bf9d91ee0718acbd9b4181f7
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-12-19 20:59:59 +0100

    chore: update .stylelintrc.json to include custom SCSS at-rule exceptions and enhance type declarations for image modules in globals.d.ts

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -12,3 +12,64 @@ declare global {
     height?: number | null;
   }
 }
+
+declare module '*.png' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module '*.svg' {
+  /**
+   * Use `any` to avoid conflicts with
+   * `@svgr/webpack` plugin or
+   * `babel-plugin-inline-react-svg` plugin.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
