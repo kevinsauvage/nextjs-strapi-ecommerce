7d40009f94f0ebb5ed3aab61af0cadc27628fbea
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-12-20 20:31:27 +0100

    refactor: update ListDisplay component styles for improved layout flexibility and add container max-widths in globals.css for better readability

--- src/components/ListDisplay.tsx
+++ src/components/ListDisplay.tsx
@@ -11,7 +11,7 @@ const ListDisplay = ({
 }) => {
   return (
     <ul
-      className={`gap-4 md:gap-5 lg:gap-7 xl:gap-8 2xl:gap-9 ${
+      className={`gap-4 md:gap-6 lg:gap-8 ${
         layout === 'grid'
           ? 'grid grid-cols-[repeat(auto-fill,minmax(170px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(300px,1fr))]  '
           : 'flex flex-col'
