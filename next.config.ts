b3da4c8b33e3b682fe6a346b9acd61751fb0046f
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-12-29 15:14:06 +0100

    refactor: update Content Security Policy to allow wildcard subdomains for Google Analytics and Shopify CDN

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,7 @@ const nextConfig: NextConfig = {
           "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
           "img-src 'self' data: blob: https://cdn.shopify.com https://res.cloudinary.com https://www.googletagmanager.com https://www.google-analytics.com",
           "font-src 'self' data: https://fonts.gstatic.com https://cdn.shopify.com",
-          "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://*.myshopify.com https://*.shopifycdn.com",
+          "connect-src 'self' https://www.googletagmanager.com https://*.google-analytics.com https://*.myshopify.com https://*.shopifycdn.com",
           "frame-src 'self' https://www.googletagmanager.com",
           "object-src 'none'",
           "base-uri 'self'",
